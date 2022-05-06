46417c0740e5968f3c046d9546ec17577723b039
Evelyne Namwoyo <evelyne@laboremus.no>
2022-05-06 16:23:57 +0100

    Updated serach endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
+from crypt import methods
 from flask import jsonify, request
 from app import app
 
 document_list = []
-
-@app.route("/index", methods=['POST'])
+# @app.route("/index", methods=['POST'])
+@app.route('/api/v1/index', methods=['POST'])
 def add_document():
     document = {
                 'title':request.json.get('title', ""),
@@ -12,4 +13,28 @@ def add_document():
                 }
     document_list.append(document)
     return jsonify({'document':document}), 201
+
+@app.route('/api/v1/search/<search_word>', methods=['GET'])
+def search_document(search_word):
+    matched_documents = []
+    related_documents = []
+    for item in document_list:
+        if((search_word in item['title']) or (search_word in item['body'])):
+            print('worrrdd')
+            matched_documents.append(item)
+
+        # for tag in item['tags']:
+        if((search_word in item['tags'])):
+            # If Item is already in the matched documents, do we need it in related?
+            related_documents.append(item)
+    documents = {
+        'matched_documents': matched_documents,
+        'related_documents': related_documents
+    }
+
+    return jsonify(documents), 200
+
+@app.route('/api/v1/documents', methods=['GET'])
+def get_documents():
+    return jsonify(document_list)
 
