08bd086c2bbed0bba99054231a543ee2042766d3
Evelyne Namwoyo <evelyne@laboremus.no>
2022-05-06 18:45:09 +0100

    Added databse tables

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 from sqlalchemy.dialects.sqlite import TEXT
 
@@ -5,10 +6,14 @@ class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(600))
     body = db.Column(db.TEXT())
-
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    tags = db.relationship('DocumentTag', backref='doc', lazy='dynamic')
     def __repr__(self):
         return '<Document {}>'.format(self.title)
 
-# class DocumentTags(db.Model):
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
+class DocumentTag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag_text = db.Column(db.String(200))
+    document_id = db.Column(db.Integer, db.ForeignKey('document.id'))
+    def __repr__(self):
+        return '<DocumentTag {}>'.format(self.tag_text)
