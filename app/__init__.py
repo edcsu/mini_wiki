ba462f2879fcc74dfe19908152049820ce45f63a
Evelyne Namwoyo <evelyne@laboremus.no>
2022-05-07 09:08:53 +0100

    updated models

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 app.config.from_object(Config)
 db  = SQLAlchemy(app)
 migrate  = Migrate(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,54 @@
-from crypt import methods
-from flask import jsonify, request
+from pydoc import doc
+from flask import jsonify, request, make_response
 from app import app
+from app import db
+from app import models
 
 document_list = []
-# @app.route("/index", methods=['POST'])
+
+
 @app.route('/api/v1/index', methods=['POST'])
 def add_document():
-    document = {
-                'title':request.json.get('title', ""),
-                'body': request.json.get('body', ""),
-                'tags':request.json.get('tags', "")
-                }
-    document_list.append(document)
-    return jsonify({'document':document}), 201
+    document = models.Document(
+        title=request.json.get('title', ""),
+        body=request.json.get('body', ""),
+    )
+    db.session.add(document)
+    db.session.commit()
 
-@app.route('/api/v1/search/<search_word>', methods=['GET'])
-def search_document(search_word):
-    matched_documents = []
-    related_documents = []
-    for item in document_list:
-        if((search_word in item['title']) or (search_word in item['body'])):
-            print('worrrdd')
-            matched_documents.append(item)
+    doc_tags = request.json.get('tags', "")
+    for tag in doc_tags:
+        tag_model = models.DocumentTag(tag_text=tag, doc=document)
+        db.session.add(tag_model)
+        db.session.commit()
+    # print(request.json.get('tags', ""), 'requestststst')
 
-        # for tag in item['tags']:
-        if((search_word in item['tags'])):
-            # If Item is already in the matched documents, do we need it in related?
-            related_documents.append(item)
-    documents = {
-        'matched_documents': matched_documents,
-        'related_documents': related_documents
-    }
+    return make_response(f"{document} successfully created!")
 
-    return jsonify(documents), 200
+
+# @app.route('/api/v1/search/<search_word>', methods=['GET'])
+# def search_document(search_word):
+#     matched_documents = []
+#     related_documents = []
+#     for item in document_list:
+#         if((search_word in item['title']) or (search_word in item['body'])):
+#             print('worrrdd')
+#             matched_documents.append(item)
+
+#         # for tag in item['tags']:
+#         if((search_word in item['tags'])):
+#             # If Item is already in the matched documents, do we need it in related?
+#             related_documents.append(item)
+#     documents = {
+#         'matched_documents': matched_documents,
+#         'related_documents': related_documents
+#     }
+
+#     return jsonify(documents), 200
+
 
 @app.route('/api/v1/documents', methods=['GET'])
 def get_documents():
-    return jsonify(document_list)
-
+    documents = models.Document.query.all()
+    documents_schema = models.DocumentSchema(many=True)
+    return jsonify(documents_schema.dump(documents))
